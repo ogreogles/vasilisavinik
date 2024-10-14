5f370db336b97fc05d93d3c5116790229aed3154
ogreogles <83167172+ogreogles@users.noreply.github.com>
2024-10-14 20:10:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
-// This function will show the image in the lightbox
 const zoomImg = function () {
-  // Create evil image clone
   let clone = this.cloneNode();
   clone.classList.remove("zoomd");
 
-  // Put evil clone into lightbox
   let lb = document.getElementById("lb-img");
   lb.innerHTML = "";
   lb.appendChild(clone);
 
-  // Show lightbox
   lb = document.getElementById("lb-back");
   lb.classList.add("show");
 };
 
 window.addEventListener("load", function () {
-  // Attach on click events to all .zoomD images
   let images = document.getElementsByClassName("zoomd");
   if (images.length > 0) {
     for (let img of images) {
       img.addEventListener("click", zoomImg);
     }
   }
-
-  // Click event to hide the lightbox
+  
   document.getElementById("lb-back").addEventListener("click", function () {
     this.classList.remove("show");
   });
